218d0aebcb62a3c6899030a88921b5292997a246
luis torres <tl224627@gmail.com>
2020-10-14 08:28:43 -0600

    add exit button

--- RentalForm.vb
+++ RentalForm.vb
@@ -2,5 +2,7 @@
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
-
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 End Class
