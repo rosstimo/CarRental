2fbe0d1797e154b180858a77a54bfe89783a68ce
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-25 08:27:17 -0600

    Add assignment header.

--- RentalForm.vb
+++ RentalForm.vb
@@ -1,6 +1,13 @@
-﻿Option Explicit On
+﻿'Elliot Heiner
+'RCET 0265
+'Fall 2021
+'Car Rental
+'https://github.com/heinelli/CarRental.git
+
+Option Explicit On
 Option Strict On
 Option Compare Binary
+
 Public Class RentalForm
 
 End Class
