dce1891eb1adbc7e868a501911826996c409f07c
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-29 08:42:53 -0600

    Add mileage cost calculations.

--- RentalForm.vb
+++ RentalForm.vb
@@ -16,8 +16,19 @@ Public Class RentalForm
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
         Dim dailyCharge As Double = 15 * CDbl(DaysTextBox.Text)
         Dim mileage As Double = CDbl(EndOdometerTextBox.Text) - CDbl(BeginOdometerTextBox.Text)
+        Dim mileageCharge As Double
 
-        TotalChargeTextBox.Text = CStr("$" & dailyCharge)
+
+
+        If mileage <= 200 Then
+            mileageCharge = 0
+        ElseIf mileage >= 201 And mileage <= 500 Then
+            mileageCharge = (mileage - 200) * 0.12
+        ElseIf mileage > 500 Then
+            mileageCharge = ((mileage - 500) * 0.1) + 36
+        End If
+
+        TotalChargeTextBox.Text = CStr("$" & dailyCharge + mileageCharge)
         TotalMilesTextBox.Text = CStr(mileage)
     End Sub
 
@@ -47,4 +58,5 @@ Public Class RentalForm
         'TotalChargeTextBox.Text = ""
 
     End Sub
+
 End Class
