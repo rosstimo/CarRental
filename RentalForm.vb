29ebc62f9a977732989684f3312b1943b917405d
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-30 14:36:33 -0600

    Add if then statements and currencyformatting to display the final cost.

--- RentalForm.vb
+++ RentalForm.vb
@@ -16,8 +16,15 @@ Public Class RentalForm
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
         Dim dailyCharge As Double = 15 * CDbl(DaysTextBox.Text)
         Dim mileage As Double = CDbl(EndOdometerTextBox.Text) - CDbl(BeginOdometerTextBox.Text)
+        Dim mileageKm As Double = mileage * 0.62
         Dim mileageCharge As Double
+        Dim cost As Double
+        Dim totalCost As Double
+        Dim discount As Double
 
+        If KilometersradioButton.Checked = True Then
+            mileage = mileageKm
+        End If
 
 
         If mileage <= 200 Then
@@ -27,9 +34,24 @@ Public Class RentalForm
         ElseIf mileage > 500 Then
             mileageCharge = ((mileage - 500) * 0.1) + 36
         End If
+        TotalMilesTextBox.Text = CStr(mileage) & " mi"
 
-        TotalChargeTextBox.Text = CStr("$" & dailyCharge + mileageCharge)
-        TotalMilesTextBox.Text = CStr(mileage)
+        cost = dailyCharge + mileageCharge
+        If AAAcheckbox.Checked = True And Seniorcheckbox.Checked = True Then
+            discount = 0.08
+            totalCost = cost - (cost * discount)
+        ElseIf AAAcheckbox.Checked = True Then
+            discount = 0.05
+            totalCost = cost - (cost * discount)
+        ElseIf Seniorcheckbox.Checked = True Then
+            discount = 0.03
+            totalCost = cost - (cost * discount)
+        Else
+            totalCost = cost
+        End If
+
+        TotalChargeTextBox.Text = FormatCurrency(totalCost)
+
     End Sub
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
