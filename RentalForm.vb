fa546751a43dda4caff33909d2950c220c93cc5d
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-30 15:07:14 -0600

    Add input validation for odometer readings.

--- RentalForm.vb
+++ RentalForm.vb
@@ -14,44 +14,60 @@ Public Class RentalForm
     End Sub
 
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
-        Dim dailyCharge As Double = 15 * CDbl(DaysTextBox.Text)
-        Dim mileage As Double = CDbl(EndOdometerTextBox.Text) - CDbl(BeginOdometerTextBox.Text)
-        Dim mileageKm As Double = mileage * 0.62
+        Dim dailyCharge As Double
+        Dim mileage As Double
+        'Dim mileageKm As Double = mileage * 0.62
         Dim mileageCharge As Double
         Dim cost As Double
         Dim totalCost As Double
         Dim discount As Double
+        Dim displayPermission As Boolean
 
-        If KilometersradioButton.Checked = True Then
-            mileage = mileageKm
+        Try
+            dailyCharge = 15 * CDbl(DaysTextBox.Text)
+            mileage = CDbl(EndOdometerTextBox.Text) - CDbl(BeginOdometerTextBox.Text)
+            displayPermission = True
+        Catch ex As Exception
+            MsgBox("Missing information")
+            displayPermission = False
+        End Try
+
+        If displayPermission = True Then
+
+            If KilometersradioButton.Checked = True Then
+                mileage = mileage * 0.62
+            End If
+
+
+
+            If mileage <= 200 Then
+                mileageCharge = 0
+            ElseIf mileage >= 201 And mileage <= 500 Then
+                mileageCharge = (mileage - 200) * 0.12
+            ElseIf mileage > 500 Then
+                mileageCharge = ((mileage - 500) * 0.1) + 36
+            End If
+            TotalMilesTextBox.Text = CStr(mileage) & " mi"
+            MileageChargeTextBox.Text = FormatCurrency(mileageCharge)
+            DayChargeTextBox.Text = FormatCurrency(dailyCharge)
+
+            cost = dailyCharge + mileageCharge
+            If AAAcheckbox.Checked = True And Seniorcheckbox.Checked = True Then
+                discount = 0.08
+                totalCost = cost - (cost * discount)
+            ElseIf AAAcheckbox.Checked = True Then
+                discount = 0.05
+                totalCost = cost - (cost * discount)
+            ElseIf Seniorcheckbox.Checked = True Then
+                discount = 0.03
+                totalCost = cost - (cost * discount)
+            Else
+                totalCost = cost
+            End If
+
+            TotalDiscountTextBox.Text = FormatCurrency(cost * discount)
+            TotalChargeTextBox.Text = FormatCurrency(totalCost)
         End If
-
-
-        If mileage <= 200 Then
-            mileageCharge = 0
-        ElseIf mileage >= 201 And mileage <= 500 Then
-            mileageCharge = (mileage - 200) * 0.12
-        ElseIf mileage > 500 Then
-            mileageCharge = ((mileage - 500) * 0.1) + 36
-        End If
-        TotalMilesTextBox.Text = CStr(mileage) & " mi"
-
-        cost = dailyCharge + mileageCharge
-        If AAAcheckbox.Checked = True And Seniorcheckbox.Checked = True Then
-            discount = 0.08
-            totalCost = cost - (cost * discount)
-        ElseIf AAAcheckbox.Checked = True Then
-            discount = 0.05
-            totalCost = cost - (cost * discount)
-        ElseIf Seniorcheckbox.Checked = True Then
-            discount = 0.03
-            totalCost = cost - (cost * discount)
-        Else
-            totalCost = cost
-        End If
-
-        TotalChargeTextBox.Text = FormatCurrency(totalCost)
-
     End Sub
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
